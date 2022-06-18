31f408098b408ccacbc48fd051c038d814743b99
vickyruud <aprajit.k@gmail.com>
2022-06-18 14:44:34 -0400

    add model for contributions

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Story = require('../models/story')
-const User = require('../models/user')
+const Story = require('../models/story');
+const User = require('../models/user');
+const Contribution = require('../models/contribution');
+
 
 
 
@@ -38,7 +40,6 @@ router.get('/users', (req, res, next) => {
 });
 
 router.post('/users', (req, res, next) => {
-  console.log(req);
   if (req.body) {
     User.create(req.body)
       .then((data) => res.json(data))
@@ -53,6 +54,31 @@ router.post('/users', (req, res, next) => {
 
 router.delete('/users/:id', (req, res, next) => {
     User.findOneAndDelete({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.get('/contributions', (req, res, next) => {
+  Contribution.find({})
+    .then((data) => res.json(data))
+    .catch(next);
+});
+
+router.post('/contributions', (req, res, next) => {
+  if (req.body) {
+    Contribution.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: 'The input field is empty',
+    });
+  }
+});
+
+
+router.delete('/contributions/:id', (req, res, next) => {
+    Contribution.findOneAndDelete({ _id: req.params.id })
     .then((data) => res.json(data))
     .catch(next);
 });
